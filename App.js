af6055be83f4553e3445c294abcd3f10f2a79f61
Cristian Wang <wwangcris1999@gmail.com>
2021-11-19 01:45:46 +0100

    Finish Fundamental Concepts

--- App.js
+++ App.js
@@ -1,21 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  TouchableOpacity,
+  Button,
+  Alert,
+  Platform,
+} from "react-native";
 
 export default function App() {
+  const handlePress = () => {
+    console.log("Text pressed");
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handlePress}>
+        Open up App.js to start working on your app!
+      </Text>
+      {/* <Image source={require("./assets/favicon.png")} /> */}
+      <TouchableOpacity>
+        <Image
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300",
+          }}
+        />
+      </TouchableOpacity>
+      <Button
+        color="orange"
+        title="Click me"
+        // SIMPLE ALERT
+        // onPress={() => alert("Lol nabbo")}
+        // CUSTOM ALERT-alert
+        onPress={() =>
+          Alert.alert("Custom title", "Custom message", [
+            { text: "Yes", onPress: () => console.log("You tapped Yes") },
+            { text: "No", onPress: () => console.log("You tapped No") },
+          ])
+        }
+        // CUSTOM ALERT-prompt -- doesn't work on Android
+        // onPress={() =>
+        //   Alert.prompt("This is a prompt", "Insert a value", (text) =>
+        //     console.log(text)
+        //   )
+        // }
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "pink",
+    // alignItems: "center",
+    // justifyContent: "center",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
 });
