880fddabacfc352350ba752f85ba4b3c19b33c44
Cristian Wang <wwangcris1999@gmail.com>
2021-11-23 21:14:54 +0100

    Fix WelcomeScreen buttons not appearing

--- App.js
+++ App.js
@@ -1,47 +1,7 @@
 import React from "react";
-import {
-  StatusBar,
-  StyleSheet,
-  Text,
-  View,
-  SafeAreaView,
-  Image,
-  TouchableOpacity,
-  Button,
-  Alert,
-  Platform,
-} from "react-native";
 
-import {
-  useDeviceOrientation,
-  useDimensions,
-} from "@react-native-community/hooks";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
-import AppText from "./app/components/AppText";
-import AppButton from "./app/components/AppButton";
 
 export default function App() {
-  const handlePress = () => {
-    console.log("Text pressed");
-  };
-
-  const { landscape } = useDeviceOrientation();
-
-  return (
-    <View style={styles.container}>
-      {/* <WelcomeScreen /> */}
-      {/* <ViewImageScreen /> */}
-      {/* <AppText>I love React Native ❤️</AppText> */}
-      <AppButton title="Login" onPress={() => console.log("Tapped")} />
-    </View>
-  );
+  return <WelcomeScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
