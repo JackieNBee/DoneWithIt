09cd3b002cc7c3885398d5fd3054c40f84650e86
Cristian Wang <wwangcris1999@gmail.com>
2021-12-11 21:23:00 +0100

    Add custom TextInput

--- App.js
+++ App.js
@@ -1,6 +1,15 @@
-import React from "react";
-import ListingsScreen from "./app/screens/ListingsScreen";
+import React, { useState } from "react";
+import { TextInput, Text } from "react-native";
+import AppTextInput from "./app/components/AppTextInput";
+
+import Screen from "./app/components/Screen";
 
 export default function App() {
-  return <ListingsScreen />;
+  const [firstName, setFirstName] = useState("");
+
+  return (
+    <Screen>
+      <AppTextInput icon="email" placeholder="Username" />
+    </Screen>
+  );
 }
