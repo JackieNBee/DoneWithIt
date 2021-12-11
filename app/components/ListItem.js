8a4d4132de0c98e7780775248af027706f4e256c
Cristian Wang <wwangcris1999@gmail.com>
2021-12-11 04:03:30 +0100

    Improve ListItem with Icon support and conditional rendering

--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -4,7 +4,14 @@ import { Swipeable } from "react-native-gesture-handler";
 import colors from "../config/colors";
 import AppText from "./AppText";
 
-function ListItem({ image, title, subTitle, onPress, renderRightActions }) {
+function ListItem({
+  title,
+  subTitle,
+  image,
+  ImageComponent, // React component (e.g. an Icon) passed as a prop so we don't pollute with extra props
+  onPress,
+  renderRightActions,
+}) {
   return (
     <Swipeable renderRightActions={renderRightActions}>
       <TouchableHighlight
@@ -14,10 +21,13 @@ function ListItem({ image, title, subTitle, onPress, renderRightActions }) {
         onPress={onPress}
       >
         <View style={styles.container}>
-          <Image style={styles.image} source={image} />
-          <View>
+          {/* Render a Component passed as a prop */}
+          {ImageComponent}
+          {/* Conditional Rendering */}
+          {image && <Image style={styles.image} source={image} />}
+          <View style={styles.detailsContainer}>
             <AppText style={styles.title}>{title}</AppText>
-            <AppText style={styles.subTitle}>{subTitle}</AppText>
+            {subTitle && <AppText style={styles.subTitle}>{subTitle}</AppText>}
           </View>
         </View>
       </TouchableHighlight>
@@ -30,11 +40,14 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     padding: 15,
   },
+  detailsContainer: {
+    marginLeft: 10,
+    justifyContent: "center",
+  },
   image: {
     width: 70,
     height: 70,
     borderRadius: 35, // to make it round must be half of width and height, cannot put "50%" like in CSS
-    marginRight: 10,
   },
   subTitle: {
     color: colors.medium,
