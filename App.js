6886b2e11820777c250b5b4bc7b4aabc15878a87
Cristian Wang <wwangcris1999@gmail.com>
2021-12-11 22:54:09 +0100

    Add custom Picker

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
 import React, { useState } from "react";
-
+import { Switch } from "react-native";
+import AppPicker from "./app/components/AppPicker";
+import AppText from "./app/components/AppText";
 import AppTextInput from "./app/components/AppTextInput";
+
 import Screen from "./app/components/Screen";
 
 export default function App() {
-  const [firstName, setFirstName] = useState("");
+  const [isNew, setIsNew] = useState(false);
 
   return (
     <Screen>
-      <AppTextInput icon="email" placeholder="Username" />
+      <AppPicker icon="apps" placeholder="Category" />
+      <AppTextInput icon="email" placeholder="Email" />
     </Screen>
   );
 }
