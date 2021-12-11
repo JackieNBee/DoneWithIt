f21f3190a31a15d0b60893c33ecc26b2019e23b6
Cristian Wang <wwangcris1999@gmail.com>
2021-12-12 00:19:56 +0100

    Add selection hadnling in AppPicker

--- App.js
+++ App.js
@@ -1,9 +1,7 @@
 import React, { useState } from "react";
-import { Switch } from "react-native";
-import AppPicker from "./app/components/AppPicker";
-import AppText from "./app/components/AppText";
-import AppTextInput from "./app/components/AppTextInput";
 
+import AppPicker from "./app/components/AppPicker";
+import AppTextInput from "./app/components/AppTextInput";
 import Screen from "./app/components/Screen";
 
 const categories = [
@@ -13,11 +11,17 @@ const categories = [
 ];
 
 export default function App() {
-  const [isNew, setIsNew] = useState(false);
+  const [category, setCategory] = useState(categories[0]);
 
   return (
     <Screen>
-      <AppPicker items={categories} icon="apps" placeholder="Category" />
+      <AppPicker
+        selectedItem={category}
+        onSelectItem={(item) => setCategory(item)}
+        items={categories}
+        icon="apps"
+        placeholder="Category"
+      />
       <AppTextInput icon="email" placeholder="Email" />
     </Screen>
   );
