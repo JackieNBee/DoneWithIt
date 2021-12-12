8d7d65fac4ee8a6df1f7b21825f6aaf16a3e2ced
Cristian Wang <wwangcris1999@gmail.com>
2021-12-12 05:05:28 +0100

    Add fix to errors showing also to fields still not being filled

--- app/components/ErrorMessage.js
+++ app/components/ErrorMessage.js
@@ -2,8 +2,9 @@ import React from "react";
 import { StyleSheet } from "react-native";
 import AppText from "./AppText";
 
-function ErrorMessage({ error }) {
-  if (!error) return null;
+function ErrorMessage({ error, visible }) {
+  // render the component only when it can be visible and there is an error to show
+  if (!visible || !error) return null;
 
   return <AppText style={styles.error}>{error}</AppText>;
 }
--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -23,28 +23,35 @@ function LoginScreen(props) {
         onSubmit={(values) => console.log(values)}
         validationSchema={validationSchema}
       >
-        {({ handleChange, handleSubmit, errors }) => (
+        {({ handleChange, handleSubmit, errors, setFieldTouched, touched }) => (
           <>
             <AppTextInput
               autoCapitalize="none"
               autoCorrect={false}
               icon="email"
               keyboardType="email-address"
+              onBlur={() => setFieldTouched("email")} // when leaving the input field set the touched state to true
               onChangeText={handleChange("email")}
               placeholder="Email"
               textContentType="emailAddress" // works only in iOS -> auto-fill field from Keychain
             />
-            <ErrorMessage error={errors.email} />
+            <ErrorMessage
+              error={errors.email}
+              // an error can be visible only when the corresponding input field has already been touched
+              // this is a fix for showing the error only to the filled fields and not triggering the ones still blank
+              visible={touched.email}
+            />
             <AppTextInput
               autoCapitalize="none"
               autoCorrect={false}
               icon="lock"
+              onBlur={() => setFieldTouched("password")}
               onChangeText={handleChange("password")}
               placeholder="Password"
               secureTextEntry
               textContentType="password" // works only in iOS -> auto-fill with password from Keychain
             />
-            <ErrorMessage error={errors.password} />
+            <ErrorMessage error={errors.password} visible={touched.password} />
             <AppButton title="Login" onPress={handleSubmit} />
           </>
         )}
