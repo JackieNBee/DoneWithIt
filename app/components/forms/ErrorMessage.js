dd32f344030e4e072352709f30eed7e437d007fa
Cristian Wang <wwangcris1999@gmail.com>
2021-12-13 05:49:56 +0100

    Improve components organization

--- app/components/forms/ErrorMessage.js
+++ app/components/forms/ErrorMessage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { StyleSheet } from "react-native";
-import AppText from "./AppText";
+import AppText from "../AppText";
 
 function ErrorMessage({ error, visible }) {
   // render the component only when it can be visible and there is an error to show
---
+++ app/components/forms/index.js
@@ -0,0 +1,11 @@
+/* 1st way to export Components */
+// import AppForm from "./AppForm";
+// import AppFormField from "./AppFormField";
+
+// export { AppForm, AppFormField };
+
+/* 2nd way to export Components */
+export { default as AppForm } from "./AppForm";
+export { default as AppFormField } from "./AppFormField";
+export { default as ErrorMessage } from "./ErrorMessage";
+export { default as SubmitButton } from "./SubmitButton";
