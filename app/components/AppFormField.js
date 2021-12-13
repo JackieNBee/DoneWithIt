b40a086306d0e176c69b77545cef4ec455478ea1
Cristian Wang <wwangcris1999@gmail.com>
2021-12-13 05:08:01 +0100

    Add AppFormField component

---
+++ app/components/AppFormField.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { useFormikContext } from "formik";
+
+import AppTextInput from "./AppTextInput";
+import ErrorMessage from "./ErrorMessage";
+
+function AppFormField({ name, ...otherProps }) {
+  // We use Formik's Context to get the data we need without passing them as props
+  const { setFieldTouched, handleChange, errors, touched } = useFormikContext();
+
+  return (
+    <>
+      <AppTextInput
+        onBlur={() => setFieldTouched(name)} // when leaving the input field set the touched state to true
+        onChangeText={handleChange(name)}
+        {...otherProps}
+      />
+      <ErrorMessage
+        error={errors[name]}
+        // an error can be visible only when the corresponding input field has already been touched
+        // this is a fix for showing the error only to the filled fields and not triggering the ones still blank
+        visible={touched[name]}
+      />
+    </>
+  );
+}
+
+export default AppFormField;
--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -7,6 +7,7 @@ import AppButton from "../components/AppButton";
 import AppTextInput from "../components/AppTextInput";
 import ErrorMessage from "../components/ErrorMessage";
 import Screen from "../components/Screen";
+import AppFormField from "../components/AppFormField";
 
 const validationSchema = Yup.object().shape({
   // label() is used for the error messages
@@ -25,33 +26,24 @@ function LoginScreen(props) {
       >
         {({ handleChange, handleSubmit, errors, setFieldTouched, touched }) => (
           <>
-            <AppTextInput
+            <AppFormField
               autoCapitalize="none"
               autoCorrect={false}
               icon="email"
               keyboardType="email-address"
-              onBlur={() => setFieldTouched("email")} // when leaving the input field set the touched state to true
-              onChangeText={handleChange("email")}
+              name="email"
               placeholder="Email"
               textContentType="emailAddress" // works only in iOS -> auto-fill field from Keychain
             />
-            <ErrorMessage
-              error={errors.email}
-              // an error can be visible only when the corresponding input field has already been touched
-              // this is a fix for showing the error only to the filled fields and not triggering the ones still blank
-              visible={touched.email}
-            />
-            <AppTextInput
+            <AppFormField
               autoCapitalize="none"
               autoCorrect={false}
               icon="lock"
-              onBlur={() => setFieldTouched("password")}
-              onChangeText={handleChange("password")}
+              name="password"
               placeholder="Password"
               secureTextEntry
               textContentType="password" // works only in iOS -> auto-fill with password from Keychain
             />
-            <ErrorMessage error={errors.password} visible={touched.password} />
             <AppButton title="Login" onPress={handleSubmit} />
           </>
         )}
