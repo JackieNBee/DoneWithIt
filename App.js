051158e23383ba0fc3a43af2b7de2f8c50dd3886
Cristian Wang <wwangcris1999@gmail.com>
2022-01-22 14:51:04 +0800

    Access Image Library and display photo on screen

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import * as ImagePicker from "expo-image-picker";
-// import * as Permissions from "expo-permissions";
 
 import Screen from "./app/components/Screen";
+import { Button, Image } from "react-native";
 
 export default function App() {
-  const requestPermission = async () => {
-    /* 
-      This is how we can use the Permissions API instead of the request method
-      we can ask for multiple permissions at the same time
-      result.granted is true if and only if every permissions has been granted
-      
-      const result = await Permissions.askAsync(Permissions.CAMERA, Permissions.LOCATION_BACKGROUND)
-      result.granted;
-    */
+  const [imageUri, setImageUri] = useState();
 
+  const requestPermission = async () => {
     const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync();
     if (!granted) alert("You need to enable permission to access the library.");
   };
@@ -23,5 +16,23 @@ export default function App() {
     requestPermission();
   }, []);
 
-  return <Screen></Screen>;
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync();
+      // if the user doesn't cancel the image selection, save the image's URI
+      if (!result.cancelled) setImageUri(result.uri);
+    } catch (error) {
+      console.log("Error reading an image", error);
+    }
+  };
+
+  return (
+    <Screen>
+      <Button title="Select image" onPress={selectImage}></Button>
+      <Image
+        source={{ uri: imageUri }}
+        style={{ width: 200, height: 200 }}
+      ></Image>
+    </Screen>
+  );
 }
